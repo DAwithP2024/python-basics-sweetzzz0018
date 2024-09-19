d0973baf27a700dcbbe6627e692634c235febf24
sweetzzz0018 <sweet250018@gmail.com>
2024-09-19 14:29:49 +0800

    grade-exercise1

--- graded_ex_1.py
+++ graded_ex_1.py
@@ -32,33 +32,87 @@ products = {
 
 
 def display_sorted_products(products_list, sort_order):
-    pass
+    def sort_key(product):
+        return product[1]
+    if sort_order == 1:
+        sorted_products = sorted(products_list, key=sort_key)
+    elif sort_order == 2:
+        sorted_products = sorted(products_list, key=sort_key, reverse=True)
+    else:
+         print("Invalid sorting option. Use 1 to sort in ascending order or 2 to sort in descending order.")
+         return products_list
+    
+    return sorted_products
 
 
 def display_products(products_list):
-    pass
+    print("Product list：")
+    for index, (product_name, price) in enumerate(products_list, start=1):
+        print(f"{index}. {product_name} ， {price}")
 
 
 def display_categories():
-    pass
+    categories = list(products.keys())
+    print("Product Categories:")
+    for index, category in enumerate(categories, start=1):
+        print(f"{index}. {category}")
 
 
 def add_to_cart(cart, product, quantity):
-    pass
+    for item in cart:
+        if item[0] == product[0]: 
+            item[2] += quantity  
+            return
+    cart.append((product[0], product[1], quantity))
 
 def display_cart(cart):
-    pass
+    print("Shopping Cart:")
+    
+    total_cost = 0 
+    for index, (product_name, price, quantity) in enumerate(cart, start=1):
+        cost = price * quantity
+        total_cost += cost
+        print(f"{index}. {product_name} - {quantity} * {price} each = {cost}")
+    
+    print(f"Total cost: {total_cost}")
+
 
 
 def generate_receipt(name, email, cart, total_cost, address):
-    pass
+    print("\n--- Receipt ---")
+    print(f"Name: {name}")
+    print(f"Email: {email}")
+    print("Shopping List:")
+    
+    for index, (product_name, price, quantity) in enumerate(cart, start=1):
+        cost = price * quantity
+        print(f"{index}. {product_name} - {quantity} * {price} each = {cost}")
+    
+    print(f"\nTotal Cost: ${total_cost}")
+    print(f"Delivery Address: {address}")
+    print("\nYour items will be delivered in 3 days. ")
+    print("\nPayment will be accepted after successful delivery.")
+    print("--- End ---")
+
 
 
 def validate_name(name):
-    pass
-
+    parts = name.strip().split()
+    
+    if len(parts) != 2:
+        return False
+    
+    first_name, last_name = parts
+    
+    if first_name.isalpha() and last_name.isalpha():
+        return True
+    else:
+        return False
+    
 def validate_email(email):
-    pass
+    if "@" in email:
+        return True
+    return False
 
 
 def main():
